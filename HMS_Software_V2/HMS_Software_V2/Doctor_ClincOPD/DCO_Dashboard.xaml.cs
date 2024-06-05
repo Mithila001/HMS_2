8df726f2c601fad033c5e1293ed5c42c83c11caf
Mithila001 <136560946+Mithila001@users.noreply.github.com>
2024-06-05 20:17:12 +0530

    2024-06-05 8.17 pm

--- HMS_Software_V2/HMS_Software_V2/Doctor_ClincOPD/DCO_Dashboard.xaml.cs
+++ HMS_Software_V2/HMS_Software_V2/Doctor_ClincOPD/DCO_Dashboard.xaml.cs
@@ -22,6 +22,31 @@ namespace HMS_Software_V2.Doctor_ClincOPD
         public DCO_Dashboard()
         {
             InitializeComponent();
+
+        }
+
+
+
+        // Button Section ------------------------------------------------------------------------------------
+        private void confirm_btn_Click(object sender, RoutedEventArgs e)
+        {
+            string patientRID = patientRID_tbx.Text.ToString();
+
+            if (General_Purpose.InputValidations.MyIsNullorempty(patientRID))
+            {
+                MessageBox.Show("Enter Patient RID", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            if(!General_Purpose.InputValidations.MyIsOnlyNumbers(patientRID))
+            {
+                MessageBox.Show("Enter only numbers", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private void reset_btn_Click(object sender, RoutedEventArgs e)
+        {
+            patientRID_tbx.Text = "";
         }
     }
 }
---
+++ HMS_Software_V2/HMS_Software_V2/General Purpose/InputValidations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS_Software_V2.General_Purpose
+{
+    public class InputValidations
+    {
+        public static bool MyIsNullorempty(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool MyIsOnlyNumbers(string value)
+        {
+            if (value.All(char.IsDigit))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
