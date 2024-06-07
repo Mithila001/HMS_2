a64d885dbb999c496eacd5d713cff5a1f6411701
Mithila001 <136560946+Mithila001@users.noreply.github.com>
2024-06-07 22:07:51 +0530

    2024-06-07 10.07 PM

--- HMS_Software_V2/HMS_Software_V2/UserCommon_Forms/LabRequests.xaml.cs
+++ HMS_Software_V2/HMS_Software_V2/UserCommon_Forms/LabRequests.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using HMS_Software_V2.Reception.R_UserControls;
+using HMS_Software_V2.UserCommon_Forms.UserControls_UCF;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +22,38 @@ namespace HMS_Software_V2.UserCommon_Forms
     /// </summary>
     public partial class LabRequests : Window
     {
+       
         public LabRequests()
         {
             InitializeComponent();
+
+            AddNewLabRequest();
+        }
+
+        private void AddNewLabRequest()
+        {
+            Debug.WriteLine("AddNewLabRequest Started");
+
+            UC_UCF_LabRequest uc_UCF_LabRequest = new UC_UCF_LabRequest();
+
+            // Pass the parent control to the user control
+            uc_UCF_LabRequest.SetParent(AddLabRequest_WrapP);
+
+            // Subscribe to the AddLabRequestClicked event
+            uc_UCF_LabRequest.AddLabRequestClicked += () =>
+            {
+                // This code will be executed when the AddLabRequestClicked event is raised
+                AddNewLabRequest();
+            };
+
+
+            AddLabRequest_WrapP.SizeChanged += (sender, e) =>
+            {
+                uc_UCF_LabRequest.Width = AddLabRequest_WrapP.ActualWidth - uc_UCF_LabRequest.Margin.Left - uc_UCF_LabRequest.Margin.Right;
+            };
+            uc_UCF_LabRequest.Width = AddLabRequest_WrapP.ActualWidth - uc_UCF_LabRequest.Margin.Left - uc_UCF_LabRequest.Margin.Right;
+
+            AddLabRequest_WrapP.Children.Add(uc_UCF_LabRequest);
         }
     }
 }
--- HMS_Software_V2/HMS_Software_V2/UserCommon_Forms/UserControls_UCF/UC_UCF_LabRequest.xaml.cs
+++ HMS_Software_V2/HMS_Software_V2/UserCommon_Forms/UserControls_UCF/UC_UCF_LabRequest.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,39 +21,25 @@ namespace HMS_Software_V2.UserCommon_Forms.UserControls_UCF
     /// </summary>
     public partial class UC_UCF_LabRequest : UserControl
     {
+        public event Action? AddLabRequestClicked;
+
         public UC_UCF_LabRequest()
         {
             InitializeComponent();
+
+            investigationTypeSearch_listBox.SelectionChanged += investigationTypeSearch_tbx_SelectionChanged;
         }
 
         private List<string> data = new List<string>
-        {
-            "Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape", "Honeydew","Ann" ,"Annyooooooooooooo"
-        };
-
-        private void SearchTextBox_KeyUp(object sender, KeyEventArgs e)
-        {
-            string query = SearchTextBox.Text.ToLower();
-            if (string.IsNullOrEmpty(query))
             {
-                SearchResultsListBox.ItemsSource = null;
-                SearchResultsPopup.IsOpen = false;
-                return;
-            }
+                "Alexander", "Amelia", "Aria", "Asher", "Ava", "Anthony", "Aiden", "Andrew", "Aurelia", "Arlo"
+            };
 
-            var filteredData = data.Where(item => item.ToLower().Contains(query)).Take(10).ToList();
-            SearchResultsListBox.ItemsSource = filteredData;
-            SearchResultsPopup.IsOpen = filteredData.Any();
-
-
-            // Assuming each item in the ListBox has a height of 20
-            SearchResultsListBox.Height = filteredData.Count * 20;
-        }
 
         private List<string> data2 = new List<string>
-        {
-            "Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape", "Honeydew","Ann" ,"Annyooooooooooooo"
-        };
+            {
+                "Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape", "Honeydew","Ann" ,"Annyooooooooooooo"
+            };
 
         private void specimentSearch_tbx_KeyUp(object sender, KeyEventArgs e)
         {
@@ -70,6 +57,128 @@ namespace HMS_Software_V2.UserCommon_Forms.UserControls_UCF
 
             specimentSearch_ListBox.Height = filteredData.Count * 20;
             specimentSearch_ListBox.Height += 5;
+        }
+
+        private void investigationTypeSearch_tbx_KeyUp(object sender, KeyEventArgs e)
+        {
+            string query = investigationTypeSearch_tbx.Text.ToLower();
+            if (string.IsNullOrEmpty(query))
+            {
+                investigationTypeSearch_listBox.ItemsSource = null;
+                investigationTypeSearch_popup.IsOpen = false;
+                return;
+            }
+
+            var filteredData = data.Where(item => item.ToLower().Contains(query)).Take(10).ToList();
+            investigationTypeSearch_listBox.ItemsSource = filteredData;
+            investigationTypeSearch_popup.IsOpen = filteredData.Any();
+
+            investigationTypeSearch_listBox.Height = filteredData.Count * 20;
+            investigationTypeSearch_listBox.Height += 5;
+        }
+
+        private void RequestType_tbx_KeyUp(object sender, KeyEventArgs e)
+        {
+            string query = RequestType_tbx.Text.ToLower();
+            if (string.IsNullOrEmpty(query))
+            {
+                RequestType_ListBox.ItemsSource = null;
+                RequestType_PopUp.IsOpen = false;
+                return;
+            }
+
+            var filteredData = data.Where(item => item.ToLower().Contains(query)).Take(10).ToList();
+            RequestType_ListBox.ItemsSource = filteredData;
+            RequestType_PopUp.IsOpen = filteredData.Any();
+
+            RequestType_ListBox.Height = filteredData.Count * 20;
+            RequestType_ListBox.Height += 5;
+        }
+
+
+
+        private void RequestType_tbx_LostFocus(object sender, RoutedEventArgs e)
+        {
+            RequestType_ListBox.ItemsSource = null;
+            RequestType_PopUp.IsOpen = false;
+        }
+
+        private void specimentSearch_tbx_LostFocus(object sender, RoutedEventArgs e)
+        {
+            RequestType_ListBox.ItemsSource = null;
+            RequestType_PopUp.IsOpen = false;
+        }
+
+        private void investigationTypeSearch_tbx_LostFocus(object sender, RoutedEventArgs e)
+        {
+            RequestType_ListBox.ItemsSource = null;
+            RequestType_PopUp.IsOpen = false;
+        }
+
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        private WrapPanel? parent;
+
+        public void SetParent(WrapPanel parent)
+        {
+            this.parent = parent;
+        }
+
+
+        bool isClicked = false;
+
+        private void AddLabRequest_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if(isClicked)
+    {
+                // Check if parent is not null
+                if (parent != null)
+                {
+                    parent.Children.Remove(this);
+                }
+                return;
+            }
+
+            isClicked = true;
+
+            // Raise the event
+            AddLabRequestClicked?.Invoke();
+            Debug.WriteLine("AddLabRequest_btn Clicked");
+
+            // Change the image source
+            var image = (Image)AddLabRequestViewbox.Child;
+            image.Source = new BitmapImage(new Uri("pack://application:,,,/HMS_Software_V2;component/Assest/icons/icons8-delete-100.png"));
+
+            // Lower the image scale
+            AddLabRequestViewbox.Width = AddLabRequestViewbox.Height = 50;
+
+            // Change the button background color to red
+            AddLabRequest_btn.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FA5882"));
+
+            // Add hover and click effects
+            AddLabRequest_btn.MouseEnter += (s, e) => AddLabRequest_btn.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF0040"));
+            AddLabRequest_btn.MouseLeave += (s, e) => AddLabRequest_btn.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FA5882"));
+            AddLabRequest_btn.PreviewMouseDown += (s, e) => AddLabRequest_btn.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF0040"));
+        }
+
+        private void investigationTypeSearch_tbx_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            // Check if an item is selected
+            if (investigationTypeSearch_listBox.SelectedItem != null)
+            {
+                // Get the selected item
+                string? selectedItem = investigationTypeSearch_listBox.SelectedItem.ToString();
+
+                // Assign the selected item's value to the TextBox
+                investigationTypeSearch_tbx.Text = selectedItem;
+
+                // Clear the ListBox's selection
+                investigationTypeSearch_listBox.SelectedItem = null;
+
+                // Close the popup
+                investigationTypeSearch_popup.IsOpen = false;
+            }
         }
     }
 }
